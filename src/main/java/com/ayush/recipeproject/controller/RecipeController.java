9a334d552727dac1c609def7e8eb670a6efe7d5a
AYUSH RAJ <acharya.ayushraj@gmail.com>
2021-09-16 13:19:47 -0400

    Data validation in RecipeCommand

--- src/main/java/com/ayush/recipeproject/controller/RecipeController.java
+++ src/main/java/com/ayush/recipeproject/controller/RecipeController.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -31,7 +34,11 @@ public class RecipeController {
         return "recipe/receptor";
     }
     @PostMapping("recipe")
-    public String saveOrUpdate(RecipeCommand recipeCommand){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> {log.debug(objectError.toString());});
+            return "recipe/receptor";
+        }
         RecipeCommand saveCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/" + saveCommand.getId() + "/show";
     }
